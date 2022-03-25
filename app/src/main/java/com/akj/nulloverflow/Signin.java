5d9a3dacaa25bcaf9cdd1b8cd41bddd3371f9840
gyte1230 <gyte1230@naver.com>
2022-03-25 17:33:33 +0900

    Collaborate merge test

--- app/src/main/java/com/akj/nulloverflow/Signin.java
+++ app/src/main/java/com/akj/nulloverflow/Signin.java
@@ -12,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class Signin extends AppCompatActivity {
     EditText signin_ID, signin_password;
     Button signin_btn1, signin_btn2;
+    String input_ID;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,7 +26,7 @@ public class Signin extends AppCompatActivity {
         signin_btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                input_ID = signin_ID.getText().toString();
             }
         });
         signin_btn2 = findViewById(R.id.signin_button2);
