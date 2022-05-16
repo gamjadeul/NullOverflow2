0e03ab6145297d15919eeba3b048677b6ab782fc
gamjadeul <gamjadeul0217@gmail.com>
2022-05-17 02:44:02 +0900

    블루투스 연결하면 DB에 적용되고, 연결 끊기면 역시 적용됨 사용중인 자리에따라 Activity선택화면 달라지고 글자색 및 배경색 변경되는 거 확인

--- app/src/main/java/com/akj/nulloverflow/RetrofitClient.kt
+++ app/src/main/java/com/akj/nulloverflow/RetrofitClient.kt
@@ -1,5 +1,8 @@
 package com.akj.nulloverflow
 
+import android.util.Log
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -8,10 +11,24 @@ object RetrofitClient {
 
     fun getClient(baseUrl: String): Retrofit? {
 
+        val logClient = OkHttpClient.Builder()
+
+        //로깅 인터셉터
+        val loggingInterceptor = HttpLoggingInterceptor(object: HttpLoggingInterceptor.Logger {
+            override fun log(message: String) {
+                Log.i("logging", "RetrofitClient - log() called / message: $message")
+            }
+        })
+
+        //로깅인터셉터 등록
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.HEADERS)
+        logClient.addInterceptor(loggingInterceptor)
+
         if(retrofit == null) {
             retrofit = Retrofit.Builder()
                 .baseUrl(baseUrl)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(logClient.build())  //해당 레트로핏에 로깅인터셉터 단 OkHttpClient붙여줌
                 .build()
         }
 
